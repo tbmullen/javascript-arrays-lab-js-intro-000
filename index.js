2e797caf8e048e08a0fdd0ddeeb73a60c6d60408
Trevor Mullen <tbhackworth@gmail.com>
2018-01-14 13:32:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const app = "I don't do much."
 
 var kittens = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendKitten(array, element) {
-  array.push(element);
+function destructivelyAppendKitten(array, name) {
+  array.push(name);
 }
